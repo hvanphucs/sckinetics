46d44ddd02abe83eb962f016dba7da438d710838
Lalit-shaktawat <126862475+Lalit-shaktawat@users.noreply.github.com>
2023-09-06 09:49:57 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,28 @@ import setuptools
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
-requirements =[
-        "os",
+
+setuptools.setup(
+    name = "sckinetics",
+    version = "1.0",
+    author = "author",
+    author_email = "author@example.com",
+    description = "short package description",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Lalit-shaktawat/scKINETICS",
+    project_urls = {
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+     install_requires=[
+	"os",
 	"time",
 	"scanpy",
 	"pandas",
@@ -39,27 +59,7 @@ requirements =[
 	"rpy2",
 	"mygene",	
 	"sparse"
-    ]
-setuptools.setup(
-    name = "sckinetics",
-    version = "1.0",
-    author = "author",
-    author_email = "author@example.com",
-    description = "short package description",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/Lalit-shaktawat/scKINETICS",
-    project_urls = {
-        "Bug Tracker": "package issues URL",
-    },
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
-     install_requires=requirements,
+		      ],
 classifiers = [
     "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
